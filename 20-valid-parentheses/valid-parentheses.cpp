6725ac070c907814e8009feb939051bd18a558a5
LaurenceYang1218 <58680451+LaurenceYang1218@users.noreply.github.com>
2024-02-16 01:10:25 +0800

    Time: 3 ms (43.89%) | Memory: 7.6 MB (25.48%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (!st.empty() && ((s[i] == ')' && st.top() == '(') || 
+                (s[i] == ']' && st.top() == '[') || 
+                (s[i] == '}' && st.top() == '{'))) {
+                    st.pop();
+            }else {
+                st.push(s[i]);
+            }
+            
+        }
+        return st.empty();
+    }
+};
