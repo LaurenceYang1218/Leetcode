57afd752b531e86bc1db3cc84d0818985c42817a
LaurenceYang1218 <58680451+LaurenceYang1218@users.noreply.github.com>
2024-02-27 09:55:36 +0800

    Time: 13 ms (49.87%) | Memory: 17 MB (31.38%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -8,7 +8,7 @@ public:
             maxNum = max(maxNum, nums[i]);
         }
         vector<int> dp(maxNum+1, 0);
-        dp[0] = numMap[0];
+        dp[0] = 0;
         dp[1] = numMap[1];
         for (int i = 2; i <= maxNum; i++) {
             dp[i] = max(dp[i-1], dp[i-2] + numMap[i]);
