28083554bccc4b9138e16dfa4f662921ad34fba9
LaurenceYang1218 <58680451+LaurenceYang1218@users.noreply.github.com>
2024-02-29 12:21:14 +0800

    Time: 360 ms (10.87%) | Memory: 149.1 MB (97.57%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        int level = 0;
+        queue<TreeNode *> treeQ;
+        treeQ.push(root);
+        while (!treeQ.empty()) {
+            int qLen = treeQ.size();
+            int tempVal = (level % 2 == 0) ? 0 : INT_MAX;
+            for (int i = 0; i < qLen; i++) {
+                TreeNode *curr = treeQ.front();
+                treeQ.pop();
+                cout << tempVal << " " << curr->val << '\n';
+                if (level % 2 == 0) {
+                    if (curr->val % 2 == 0 || curr->val <= tempVal) {
+                        return false;
+                    }
+                }else {
+                    if (curr->val % 2 != 0 || curr->val >= tempVal) {
+                        return false;
+                    }
+                }
+                if (curr->left != nullptr) {
+                    treeQ.push(curr->left);
+                }
+                if (curr->right != nullptr) {
+                    treeQ.push(curr->right);
+                }
+                tempVal = curr->val;
+            }
+            level++;
+        }
+        return true;
+    }
+};
