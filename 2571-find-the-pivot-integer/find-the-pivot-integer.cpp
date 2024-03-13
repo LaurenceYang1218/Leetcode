84ba2e61961f369ee7875aeceda5c0c338667e05
LaurenceYang1218 <58680451+LaurenceYang1218@users.noreply.github.com>
2024-03-13 08:58:58 +0800

    Time: 3 ms (51.39%) | Memory: 7.3 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -3,7 +3,7 @@ public:
     int pivotInteger(int n) {
         int allSum = n * (n+1) / 2;
         for (int i = 1; i <= n; i++) {
-            int prefixSum = i * (i + 1) / 2;
+            int prefixSum = i * (i+1) / 2;
             int suffix = allSum - prefixSum + i;
             if (prefixSum == suffix) {
                 return i;
