fcd2e7096b819dfcf0d70a56781b9d7a28211785
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-07-23 10:28:31 +0800

    Time: 5 ms (68.31%) | Memory: 15 MB (37.05%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int, int> frequency;
+        unordered_map<int, int> freqMap;
         for (auto it : nums) {
-            frequency[it]++;
+            freqMap[it]++;
         }
-        vector<pair<int, int>> freqVec(frequency.begin(), frequency.end());
-  
-        sort(freqVec.begin(), freqVec.end(), [](pair<int, int>& p1, pair<int, int>& p2) {
-            if (p1.second < p2.second) {
-                return true;
-            } else if (p1.second == p2.second) {
+        vector<pair<int, int>> freqList(freqMap.begin(), freqMap.end());
+        sort(freqList.begin(), freqList.end(), [](pair<int, int>& p1, pair<int, int>& p2){
+            if (p1.second != p2.second) {
+                return p1.second < p2.second;
+            }else {
                 return p1.first > p2.first;
-            } else {
-                return false;
             }
         });
-
         vector<int> res;
-        for (auto it : freqVec) {
-            for (int cnt = 0; cnt < it.second; cnt++) {
-                res.push_back(it.first);
+        for (auto [num, count] : freqList) {
+            for (int i = 0; i < count; i++) {
+                res.push_back(num);
             }
         }
         return res;
