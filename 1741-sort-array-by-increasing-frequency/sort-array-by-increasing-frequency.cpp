d2c7e63d53bb4d8cbbd5b62ea3fe38ed6029e5fa
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-07-23 10:20:23 +0800

    Time: 7 ms (55.39%) | Memory: 15 MB (37.05%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> frequency;
+        for (auto it : nums) {
+            frequency[it]++;
+        }
+        vector<pair<int, int>> freqVec(frequency.begin(), frequency.end());
+  
+        sort(freqVec.begin(), freqVec.end(), [](pair<int, int>& p1, pair<int, int>& p2) {
+            if (p1.second < p2.second) {
+                return true;
+            } else if (p1.second == p2.second) {
+                return p1.first > p2.first;
+            } else {
+                return false;
+            }
+        });
+
+        for (auto it : freqVec) {
+            cout << it.first << " " << it.second << '\n'; 
+        }
+        vector<int> res;
+        for (auto it : freqVec) {
+            for (int cnt = 0; cnt < it.second; cnt++) {
+                res.push_back(it.first);
+            }
+        }
+        return res;
+    }
+};
