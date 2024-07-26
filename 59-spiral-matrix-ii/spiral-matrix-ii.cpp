ef0f29881b69f207f4837e0acefd60f242a052f3
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-07-26 14:55:56 +0800

    Time: 0 ms (100.00%) | Memory: 8 MB (14.35%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int>(n));
+        int cnt = 1, loopCnt = 0;
+        
+        for (int idx = n-1; idx > 0; idx -= 2) {
+            cout << idx << '\n';
+            for (int j = 0; j < idx; j++) {
+                res[loopCnt][j+loopCnt] = cnt++;
+                cout << loopCnt << ' ' << j+loopCnt << '\n';
+            }
+            for (int j = 0; j < idx; j++) {
+                res[j+loopCnt][n-1-loopCnt] = cnt++;
+                cout << j+loopCnt << ' ' << n-1-loopCnt << '\n';
+            }
+            for (int j = idx; j >= 1; j--) {
+                res[n-1-loopCnt][j+loopCnt] = cnt++;
+                cout << n-1-loopCnt << ' ' << j+loopCnt << '\n';
+            }
+            for (int j = idx; j >= 1; j--) {
+                res[j+loopCnt][loopCnt] = cnt++;
+                cout << j+loopCnt << ' ' << loopCnt << '\n';
+            }
+            loopCnt++;
+        }
+        if (n % 2 == 1) {
+            res[loopCnt][loopCnt] = cnt;
+        }
+
+        // for (size_t i = 0; i < res.size(); i++) {
+        //     for (size_t j = 0; j < res.size(); j++) {
+        //         cout << res[i][j] << ' ';
+        //     }
+        //     cout << '\n';
+        // }
+        return res;
+    }
+};
