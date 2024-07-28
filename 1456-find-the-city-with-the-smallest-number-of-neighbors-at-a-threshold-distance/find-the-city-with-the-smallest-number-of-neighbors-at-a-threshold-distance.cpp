57d96d14c0f3c5f31d750c4f17b8dc0c8fa37c94
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-07-28 12:08:45 +0800

    Time: 25 ms (63.70%) | Memory: 15.5 MB (81.35%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -14,11 +14,9 @@ public:
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    
                     if (distance[i][j] > distance[i][k] + distance[k][j]) {
                         distance[i][j] = distance[i][k] + distance[k][j];
                     }
-                
                 }
             }
         }
