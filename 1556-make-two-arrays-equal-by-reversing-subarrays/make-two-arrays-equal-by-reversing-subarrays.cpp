78c9daf8e8e560ef5ea62a0ca72bac2a50d07235
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-08-03 12:07:52 +0800

    Time: 9 ms (63.29%) | Memory: 17.5 MB (57.40%) - LeetSync

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(arr.begin(), arr.end()); 
+        sort(target.begin(), target.end());
+        for (size_t i = 0; i < target.size(); i++) {
+            if (target[i] != arr[i]) 
+                return false;
+        }
+        return true;
+    }
+};
