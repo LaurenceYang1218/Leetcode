79edc58dc0ffbb58615f9ae82c4dfdb62f906c36
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-09-19 10:43:19 +0800

    Time: 4 ms (46.04%) | Memory: 9 MB (93.56%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-
+    unordered_map<string, vector<int>> memo;
     vector<int> diffWaysToCompute(string expression) {
         vector<int> ret;
+        if (memo.find(expression) != memo.end()) {
+            return memo[expression];
+        }
         for (int i = 0; i < expression.size(); i++) {
             if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*') {
                 vector<int> leftResult = diffWaysToCompute(expression.substr(0, i));
@@ -30,6 +33,7 @@ public:
         if (ret.empty()) {
             ret.push_back(stoi(expression));
         }
+        memo[expression] = ret;
         return ret;
     }
 };
