73de18fc778966997aa373fc7ca15dac8853058c
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-09-24 09:11:45 +0800

    Time: 590 ms (18.05%) | Memory: 170.2 MB (38.93%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -4,19 +4,19 @@ public:
         int ans = 0;
         unordered_map<string, int> prefixMap;
         for (int i = 0; i < arr1.size(); i++) {
-            string str1 = to_string(arr1[i]);
-            for (int j = 1; j <= str1.length(); j++) {
-                prefixMap[str1.substr(0, j)]++;
+            string numStr1 = to_string(arr1[i]);
+            for (int j = 1; j <= numStr1.length(); j++) {
+                string prefixStr = numStr1.substr(0, j);
+                prefixMap[prefixStr] = j;
             }
         }
-        
         for (int i = 0; i < arr2.size(); i++) {
-            string str2 = to_string(arr2[i]);
-            for (int j = 1; j <= str2.length(); j++) {
-                if (prefixMap.find(str2.substr(0, j)) == prefixMap.end()) {
-                    break;
+            string numStr2 = to_string(arr2[i]);
+            for (int j = 1; j <= numStr2.length(); j++) {
+                string prefixStr = numStr2.substr(0, j);
+                if (prefixMap.find(prefixStr) != prefixMap.end()) {
+                    ans = max(ans, prefixMap[prefixStr]);
                 }
-                ans = max(ans, j);
             }
         }
         return ans;
