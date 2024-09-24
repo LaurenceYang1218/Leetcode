e2663dfb064bca6c2ea5968b8ae03a3754ddc355
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-09-24 09:35:05 +0800

    Time: 237 ms (81.82%) | Memory: 160.8 MB (56.99%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,23 +1,61 @@
+class TrieNode {
+public:
+    TrieNode* childs[10];
+    TrieNode() {
+        for (int i = 0; i < 10; i++) {
+            childs[i] = NULL;
+        }
+    }
+    bool contains(char ch) {
+        return (childs[ch - '0'] != NULL);
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string str) {
+        TrieNode* curr = root;
+        for (char ch : str) {
+            if (!curr->contains(ch)) {
+                curr->childs[ch - '0'] = new TrieNode();
+            }
+            curr = curr->childs[ch - '0'];
+        }
+        return;
+    }
+
+    int commonPrefix(string str) {
+        int ret = 0;
+        TrieNode* curr = root;
+        for (char ch : str) {
+            curr = curr->childs[ch - '0'];
+            if (curr == NULL) {
+                return ret;
+            }
+            ret++;
+        }
+        return ret;
+    }
+};
+
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         int ans = 0;
-        unordered_map<string, int> prefixMap;
+        Trie tr;
         for (int i = 0; i < arr1.size(); i++) {
-            string numStr1 = to_string(arr1[i]);
-            for (int j = 1; j <= numStr1.length(); j++) {
-                string prefixStr = numStr1.substr(0, j);
-                prefixMap[prefixStr] = j;
-            }
+            string str = to_string(arr1[i]);
+            tr.insert(str);
         }
+        
         for (int i = 0; i < arr2.size(); i++) {
-            string numStr2 = to_string(arr2[i]);
-            for (int j = 1; j <= numStr2.length(); j++) {
-                string prefixStr = numStr2.substr(0, j);
-                if (prefixMap.find(prefixStr) != prefixMap.end()) {
-                    ans = max(ans, prefixMap[prefixStr]);
-                }
-            }
+            string str = to_string(arr2[i]);
+            ans = max(ans, tr.commonPrefix(str));
         }
         return ans;
     }
