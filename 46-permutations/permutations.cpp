c3f47416d628a3d36639ea8de0e1d304ae96a894
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-09-30 22:11:00 +0800

    Time: 0 ms (100.00%) | Memory: 9.5 MB (50.03%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void solve(int start, vector<int> nums, vector<int> vec, int currLen) {
+
+    void solve(vector<int>& nums, vector<int>& vec, int currLen) {
         if (currLen == nums.size()) {
             ans.push_back(vec);
             return;
         }
-        for (int i = start; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
             if (find(vec.begin(), vec.end(), nums[i]) == vec.end()) {
                 vec.push_back(nums[i]);
-                solve(start, nums, vec, currLen + 1);
+                solve(nums, vec, currLen + 1);
                 vec.pop_back();
             }
         }
@@ -17,7 +18,7 @@ public:
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> vec;
-        solve(0, nums, vec, 0);
+        solve(nums, vec, 0);
         return ans;
     }
 };
