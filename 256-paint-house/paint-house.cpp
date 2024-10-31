8263e46b47bc7736b98330b9c618f0a128183dbc
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-10-31 22:09:32 +0800

    Time: 0 ms (100.00%) | Memory: 12.6 MB (89.92%) - LeetSync

---
+++ 256-paint-house/paint-house.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        if (n == 1) {
+            return min(costs[0][0], min(costs[0][1], costs[0][2]));
+        }
+        for (int i = n-2; i >= 0; i--) {
+            costs[i][0] += min(costs[i+1][1], costs[i+1][2]);
+            costs[i][1] += min(costs[i+1][0], costs[i+1][2]);
+            costs[i][2] += min(costs[i+1][0], costs[i+1][1]); 
+        }
+        return min(costs[0][0], min(costs[0][1], costs[0][2]));
+    }
+};
