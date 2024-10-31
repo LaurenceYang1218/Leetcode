1200323f6498631a875084fa6aaaec99f38bc519
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-10-31 16:32:29 +0800

    Time: 36 ms (21.02%) | Memory: 74.4 MB (52.67%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -7,15 +7,16 @@ public:
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
         }
+
         for (int j = 1; j < n; j++) {
             for (int i = 0; i < m; i++) {
                 if (grid[i][j] > grid[i][j-1] && dp[i][j-1] > 0) {
                     dp[i][j] = max(dp[i][j], dp[i][j-1] + 1);
                 }
-                if (i - 1 >= 0 && grid[i][j] > grid[i-1][j-1] && dp[i-1][j-1] > 0) {
+                if (i-1 >= 0 && grid[i][j] > grid[i-1][j-1] && dp[i-1][j-1] > 0) {
                     dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1);
                 }
-                if (i + 1 < m && grid[i][j] > grid[i+1][j-1] && dp[i+1][j-1] > 0) {
+                if (i+1 < m && grid[i][j] > grid[i+1][j-1] && dp[i+1][j-1] > 0) {
                     dp[i][j] = max(dp[i][j], dp[i+1][j-1] + 1);
                 }
                 ret = max(ret, dp[i][j]-1);
