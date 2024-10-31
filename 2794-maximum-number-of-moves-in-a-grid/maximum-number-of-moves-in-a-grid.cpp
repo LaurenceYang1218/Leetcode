6906c09a07a704433679aeab2d800bccee36d129
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-10-31 16:22:40 +0800

    Time: 27 ms (32.99%) | Memory: 74.3 MB (52.67%) - LeetSync

---
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxMoves(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int ret = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] > grid[i][j-1] && dp[i][j-1] > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i][j-1] + 1);
+                }
+                if (i - 1 >= 0 && grid[i][j] > grid[i-1][j-1] && dp[i-1][j-1] > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1);
+                }
+                if (i + 1 < m && grid[i][j] > grid[i+1][j-1] && dp[i+1][j-1] > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i+1][j-1] + 1);
+                }
+                ret = max(ret, dp[i][j]-1);
+            }
+        }
+        return ret;
+    }
+};
