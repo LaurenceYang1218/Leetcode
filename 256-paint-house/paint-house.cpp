e3ac1470460afeea9f8803ee3beeb54d487c4b73
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-11-01 01:33:58 +0800

    Time: 0 ms (100.00%) | Memory: 12.6 MB (89.92%) - LeetSync

--- 256-paint-house/paint-house.cpp
+++ 256-paint-house/paint-house.cpp
@@ -5,11 +5,11 @@ public:
         if (n == 1) {
             return min(costs[0][0], min(costs[0][1], costs[0][2]));
         }
-        for (int i = n-2; i >= 0; i--) {
-            costs[i][0] += min(costs[i+1][1], costs[i+1][2]);
-            costs[i][1] += min(costs[i+1][0], costs[i+1][2]);
-            costs[i][2] += min(costs[i+1][0], costs[i+1][1]); 
+        for (int i = 1; i < n; i++) {
+            costs[i][0] += min(costs[i-1][1], costs[i-1][2]);
+            costs[i][1] += min(costs[i-1][0], costs[i-1][2]);
+            costs[i][2] += min(costs[i-1][0], costs[i-1][1]); 
         }
-        return min(costs[0][0], min(costs[0][1], costs[0][2]));
+        return min(costs[n-1][0], min(costs[n-1][1], costs[n-1][2]));
     }
 };
