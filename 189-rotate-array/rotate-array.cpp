7e73f0a09291925eb230087fcbef905d481c050c
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-11-04 12:13:32 +0800

    Time: 0 ms (100.00%) | Memory: 28.3 MB (11.11%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> tmp(n);
+        for (int i = 0; i < n; i++) {
+            tmp[(i + k) % n] = nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = tmp[i];
+        }
+        return;
+    }
+};
