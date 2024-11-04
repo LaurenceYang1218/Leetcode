e2b0893a09af0df97f974de0f89fe070fdeef407
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-11-04 12:06:02 +0800

    Time: 7 ms (18.32%) | Memory: 28 MB (25.30%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ans = nums[0], cnt = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (ans == nums[i]) {
-                cnt++;
-            }else {
-                if (cnt > 0) {
-                    cnt--;
-                }else {
-                    ans = nums[i];
-                    cnt++;
-                }
-            }
-        }
-        return ans;
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
     }
 };
