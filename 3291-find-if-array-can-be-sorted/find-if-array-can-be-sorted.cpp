f85bfec6c46829a8fef984f30c54631f12ddd632
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-11-06 13:07:37 +0800

    Time: 99 ms (5.04%) | Memory: 31.1 MB (86.44%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        int currBits = __builtin_popcount(nums[0]);
+        int currMaxNum = nums[0];
+        int currMinNum = nums[0];
+        int prevMaxNum = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (__builtin_popcount(nums[i]) == currBits) {
+                currMaxNum = max(currMaxNum, nums[i]);
+                currMinNum = min(currMinNum, nums[i]);
+                cout << currMinNum << " " << currMaxNum << endl;
+            }else {
+                if (currMinNum < prevMaxNum) {
+                    return false;
+                }
+                prevMaxNum = currMaxNum;
+                currBits = __builtin_popcount(nums[i]);
+                currMaxNum = nums[i];
+                currMinNum = nums[i];
+            }
+        } 
+        if (currMinNum < prevMaxNum) {
+            return false;
+        }
+        return true; 
+    }
+};
