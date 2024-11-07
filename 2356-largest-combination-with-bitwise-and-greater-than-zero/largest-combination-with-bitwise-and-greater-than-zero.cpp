ae684c246c37e2a788c74162cddbd319f502930b
YANG, CHUO-MIN <58680451+LaurenceYang1218@users.noreply.github.com>
2024-11-07 09:21:47 +0800

    Time: 49 ms (11.19%) | Memory: 60.3 MB (32.98%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-        vector<int> bitCnt(24, 0);
-        for (int i = 0; i < 24; i++) {
+        vector<int> bitCnt(32, 0);
+        for (int i = 0; i < 32; i++) {
             for (int j = 0; j < candidates.size(); j++) {
                 int num = candidates[j];
                 if ((num & (1 << i)) != 0) {
